4a9b5622e78d1cb905112e73e3d1f0c6bb72e48d
Aasesi <112279862+WilczynskiMikolaj@users.noreply.github.com>
2026-01-25 03:07:43 +0100

    Version 0.4 - Added loras tab, start of restructure of model loader class to use polymorphism

--- src/sdxl_image_generator/ui/gradio_app.py
+++ src/sdxl_image_generator/ui/gradio_app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from sdxl_image_generator.utils.utils import get_all_directory_elements
+from sdxl_image_generator.sdxl_model_pipeline.model_loader import ModelLoader
 
 
 def create_ui():
@@ -7,10 +8,12 @@ def create_ui():
     lora_names = get_all_directory_elements("loras")
     
     with gr.Blocks(fill_width=True, fill_height=True) as demo:
+        lora_element_state = gr.State([{"name":lora, "enabled": False} for lora in lora_names])
+        model_loader = ModelLoader()
 
         gr.Markdown("# SDXL-CLI-GENERATOR GUI")
-        with gr.Row(equal_height=True, scale=1):
-            with gr.Tab("Generation"):
+        with gr.Row(equal_height=True):
+            with gr.Tab("Generation", scale=1):
 
                 models_dropdown = gr.Dropdown(choices=model_checkpoint_names, label="Model Checkpoint", interactive=True)
                 positive_prompt = gr.Textbox(label="Positive Prompt", lines=6)
@@ -25,8 +28,26 @@ def create_ui():
                 generate_button = gr.Button("Generate")
 
             with gr.Tab("Lora", scale=1):
-                models_dropdown = gr.Dropdown(label="Model Checkpoint")
+                @gr.render(inputs=lora_element_state)
+                def render_lora_selection(lora_list):
+                    for lora in lora_list:
+                        lora_checkbox = gr.Checkbox(interactive=True, label=lora["name"])
+                        gr.Slider(0.0, 2.0, value=0.8, step=0.05, label="Adapter Weight", interactive=lora["enabled"])
+                        def select_lora(lora=lora):
+                            if lora["enabled"] == True:
+                                lora["enabled"] = False
+                            else:
+                                lora["enabled"] = True
+                            return lora_list
+                        lora_checkbox.select(select_lora, None, lora_element_state)
+                        
+                    
+                    
             with gr.Column(scale=4):
                 gallery = gr.Gallery(preview=True, object_fit="contain")
+
+        @generate_button.click(inputs=[models_dropdown, positive_prompt, negative_prompt, width, height, inference_steps, guidance_scale, images_per_prompt, lora_element_state], outputs=gallery)
+        def generate():
+            return None
 
     return demo
